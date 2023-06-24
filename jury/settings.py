9de6d44366cc767935d70c47e4ffe8364d324aa6
Celeste Roselli <celesteroselli@Celestes-MacBook-Pro.local>
2023-06-23 22:45:52 -0400

    connected postgres database

--- jury/settings.py
+++ jury/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'jury.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'xZNpWX3jVIGLeEJIA4yS',
+        'HOST': 'containers-us-west-55.railway.app',
+        'PORT': '5849',
     }
 }
 
